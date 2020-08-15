e41eb02538a580be5d639878dc8e3c1df3f4f5f3
DeWitt Kane <dewitt.kane@gmail.com>
2020-08-14 22:09:37 -0500

    toggle of image/description done.  formatting is terrible.

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 class GalleryItem extends Component {
+
+    state = {
+        showImage: true
+    }
+
+    toggleState = () => {
+        this.setState({
+            showImage: !this.state.showImage
+        })
+    }
+
     render() {
         return(
-            <div><img src={this.props.image.path} alt={this.props.image.description}></img></div>
+            <div onClick={this.toggleState}>
+            {this.state.showImage ? <img src={this.props.image.path} alt={this.props.image.description}/> : <p>{this.props.image.description}</p>}
+            </div>
         );
     };
 };
